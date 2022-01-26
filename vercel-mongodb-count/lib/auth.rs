6fac5b7c4cc1b2daf728153f7c6fd4980e545656
BORBER <borber.drum@gmail.com>
2022-01-26 17:45:34 +0800

    [mod] auth_code 移入环境变量

--- vercel-mongodb-count/lib/auth.rs
+++ vercel-mongodb-count/lib/auth.rs
@@ -1,15 +1,9 @@
 use serde::{Deserialize, Serialize};
 use crate::api::Status;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct AuthConfig {
-    pub auth_code: String
-}
-
 pub fn check(auth_code: String) -> bool {
-    let str_val = include_str!("auth.toml"); // 请按照 `auth-example.toml` 的格式设置
-    let config = toml::from_str::<AuthConfig>(&str_val).expect("Parsing failed, please check your profile structure");
-    auth_code.as_str() == config.auth_code.as_str()
+    let env_auth_code = env!("AUTH_CODE");
+    auth_code.as_str() == env_auth_code
 }
 
 pub fn check_failed() -> Status {
